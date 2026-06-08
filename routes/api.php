2e7a5e0bb111b5d3e6a4fa43e1367790bdd18ea0
ahmadsidikrofi <rofidragon71@gmail.com>
2026-06-08 16:38:20 +0700

    add cors * and set scheduler to start at 7 am every day

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::get('/gold-prices/all', [GoldPriceController::class, 'index']);
     Route::get('/gold-prices/highlight', [GoldPriceController::class, 'highlight']);
+    Route::get('/gold-prices/chart-data', [GoldPriceController::class, 'history']);
 });
